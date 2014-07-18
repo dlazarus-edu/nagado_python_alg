647b3821994412406de4b8754a7501c629ee1470
Nagado <nagado@yandex.ru>
2014-07-18 17:10:01 -0500

    updated

--- SortTester.py
+++ SortTester.py
@@ -1,4 +1,4 @@
-import time, random
+import time, random, sys
 
 class SortTester:
     def __init__(self, length, lmax = 13579, lmin = 0):
@@ -22,7 +22,22 @@ class SortTester:
         if self.check():
             print "Something is wrong"
         else:
-            print "List is sorted. Time of my work: ", time.clock() - begin
+            print "Time of my work: ", time.clock() - begin
 ##            print self.data
 
+try:
+    sorter = getattr(__import__(sys.argv[1]), sys.argv[1])
+except IndexError:
+    print "Sorry, but you need to add sorting module which you are going to test as second argument"
+    exit()
+except ImportError:
+    print "I can't find file to import. Maybe you wrote '.py' in the end, or you misspeled. Check yourself and try again!"
+    exit()
+except AttributeError:
+    print "There is no attribute ", sys.argv[1], ". Try again."
+    exit()
 
+for i in [100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000]:
+    test = SortTester(i)
+    print i, " elements in a list.",
+    test.run(sorter)
