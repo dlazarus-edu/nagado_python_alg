9a4fd10bdc3a651e5ada96d54af38a51c97c3fcb
Nagado <nagado@yandex.ru>
2014-07-25 15:59:02 -0500

    new sort type! Smiley :)

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(data):
+    for k in range(len(data)):
+        min = data[k]
+        
+        for i in range(k, len(data)):
+              if data[i] < min:
+                  min = data[i]
+                  
+        data.remove(min)
+        data.insert(k, min)
+
